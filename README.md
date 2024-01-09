bc31198d5a50b1d518287e9a47959ae7c1787c17
bjfu-qwh <87230251+bjfu-qwh@users.noreply.github.com>
2024-01-09 21:53:11 +0800

    feat:准备提交仓库

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 配置要求
 
-- node版本： [v18.9.1](https://nodejs.org/dist/v18.9.1/)。
+- node版本： [v16.9.1](https://nodejs.org/dist/v16.9.1/)。
 
 其余模块根据package.json和package-lock.json即可锁定具体版本。初次使用项目时输入命令
 
@@ -10,4 +10,8 @@
 npm install
 ```
 
-即可下载对应依赖。
+即可下载对应依赖。node的版本**不宜过高**，尤其不推荐**20往上的**。16系列和18系列相对合适。
+
+## 组建依赖
+
+目前的项目已经实现Element Plus的自动引入和Vue Router的组装。
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,15 @@ import StoreComponent from "../components/Store/StoreComponent.vue";
 
 const HomeComponent = () => import('../components/Home/HomeComponent.vue');
 
+/**
+ * vue-router的路由配置菜单。如果要添加组件：
+ * 在Home的children中，按照如下规则添加JavaScript对象
+ * {
+ *     name:组件的名称，最好和components子目录名称一致
+ *     component：仿照上面的import引入目标组件
+ *     path：以/开头写路径，router.push参数为此
+ * }
+ */
 const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
